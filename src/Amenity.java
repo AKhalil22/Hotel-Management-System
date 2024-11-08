7dbb3c4e59be0e63b3a1c27187aeb622e70ade15
AK <ammarkhalil786@gmail.come>
2024-11-08 18:59:50 -0500

    Added pricing to Amenities + More Options

--- src/Amenity.java
+++ src/Amenity.java
@@ -1,10 +1,27 @@
 public enum Amenity {
-    WIFI,
-    BREAKFAST,
-    POOL_ACCESS,
-    GYM_ACCESS,
-    PARKING,
-    ROOM_SERVICE,
-    MINI_FRIDGE
+    WIFI(0.0),
+    BREAKFAST(0.0),
+    POOL_ACCESS(0.0),
+    GYM_ACCESS(0.0),
+    PRIORITY_PARKING(75.0),
+    ROOM_SERVICE(50.0), // 24-HOURS
+    MINI_FRIDGE(0.0),
+    BIKE_RENTAL(0.0),
+    SPA(100.0),
+    MEALCARD(250.0); // Access to free meals (local eateries)
+
     // Add any other amenities here
+
+    double price; // Optionally make "final" to prevent price changes
+
+    // Setter (Enum)
+    Amenity(double price) {
+        this.price = price;
+    }
+
+    // Getter (Enum)
+    public double getPrice() {
+        return price;
+    }
+
 }
