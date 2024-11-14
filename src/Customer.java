7cb09997227fc454c9d87be17d55daf649c496b2
Ammar Khalil <ammarkhalil786@gmail.com>
2024-11-14 13:27:02 -0500

    Hashset for amenities moved under customer class from Room class.

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Customer {
     // Customer Parameters/Variables
     private String name;
     private Integer cardNumber;
     private Boolean loyaltyMember;
+    private Set<Amenity> amenities;
 
     public Customer(String name, Integer cardNumber, Boolean loyaltyMember) {
         this.name = name;
         this.cardNumber = cardNumber;
         this.loyaltyMember = loyaltyMember;
+        this.amenities = new HashSet<>(); // Initialize an empty Hashset of amenities
     }
 
     // Getter & Setters
@@ -34,4 +39,20 @@ public class Customer {
     public void setLoyaltyMember(Boolean loyaltyMember) {
         this.loyaltyMember = loyaltyMember;
     }
+
+    // Method to add an amenity
+    public void addAmenity(Amenity amenity) {
+        amenities.add(amenity);
+    }
+
+    // Method to remove an amenity
+    public void removeAmenity(Amenity amenity) {
+        amenities.remove(amenity);
+    }
+
+    // Method to check if a room has a specific amenity
+    public boolean hasAmenity(Amenity amenity) {
+        return amenities.contains(amenity);
+    }
+
 }
--- src/Room.java
+++ src/Room.java
@@ -4,33 +4,15 @@ import java.util.Set;
 public class Room {
     private int roomNumber;
     private String roomType; // e.g., "Single", "Double", "Suite"
-    private Set<Amenity> amenities;
 
     public Room(int roomNumber, String roomType) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
-        this.amenities = new HashSet<>(); // Initialize an empty Hashset of amenities
     }
 
     // Method to get room type
     public int getRoomNumber() { return roomNumber; }
     // Method to get room type (i.e. 2 bedroom, 1 bedroom)
     public String getRoomType() { return roomType; }
-    // Method to add an amenity
-    public void addAmenity(Amenity amenity) {
-        amenities.add(amenity);
-    }
-
-    // Method to remove an amenity
-    public void removeAmenity(Amenity amenity) {
-        amenities.remove(amenity);
-    }
-
-    // Method to check if a room has a specific amenity
-    public boolean hasAmenity(Amenity amenity) {
-        return amenities.contains(amenity);
-    }
-
-
 
 }
